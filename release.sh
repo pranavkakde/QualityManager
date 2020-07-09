586a54fef7494b1e5922430a5e351600fa1413ce
Pranav Kakde <pranavkakde@gmail.com>
2020-07-10 02:52:27 +1000

    updated build files

--- release.sh
+++ release.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-version=`cat VERSION`
-imagelist=( 'pranavkakde/qualitymanager_mainbuild' 'pranavkakde/qualitymanager_auth-services' 'pranavkakde/qualitymanager_gateway-services' 'pranavkakde/qualitymanager_release-services' 'pranavkakde/qualitymanager_project-services' 'pranavkakde/qualitymanager_testcase-services' 'pranavkakde/qualitymanager_client-services' 'pranavkakde/qualitymanager_testsuite-services' 'pranavkakde/qualitymanager_defect-services' 'pranavkakde/qualitymanager_user-services')
+#imagelist=( 'pranavkakde/qualitymanager_mainbuild' 'pranavkakde/qualitymanager_auth-services' 'pranavkakde/qualitymanager_gateway-services' 'pranavkakde/qualitymanager_release-services' 'pranavkakde/qualitymanager_project-services' 'pranavkakde/qualitymanager_testcase-services' 'pranavkakde/qualitymanager_client-services' 'pranavkakde/qualitymanager_testsuite-services' 'pranavkakde/qualitymanager_defect-services' 'pranavkakde/qualitymanager_user-services')
+imagelist=($(docker images | awk '{ if ($1 ~ /pranavkakde\/qualitymanager*/) { print $1 } }'))
 for i in "${imagelist[@]}"
 do	
-    docker push $i:latest
-    docker push $i:$version
+    docker push $i:latest    
     docker push $i:$buildversion
 done
