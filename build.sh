a19ecec5bf174bf1b9960e7228cfd2342e444ab7
Pranav Kakde <pranavkakde@gmail.com>
2020-07-10 01:54:48 +1000

    updated docker versioning

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker run --rm -v "$PWD":/app treeder/bump patch
+version=`cat VERSION`
+#run docker compose
+docker-compose build
+#tag images for latest version
+imagelist=( 'pranavkakde/qualitymanager_mainbuild' 'pranavkakde/qualitymanager_auth-services' 'pranavkakde/qualitymanager_gateway-services' 'pranavkakde/qualitymanager_release-services' 'pranavkakde/qualitymanager_project-services' 'pranavkakde/qualitymanager_testcase-services' 'pranavkakde/qualitymanager_client-services' 'pranavkakde/qualitymanager_testsuite-services' 'pranavkakde/qualitymanager_defect-services' 'pranavkakde/qualitymanager_user-services')
+for i in "${imagelist[@]}"
+do	
+    docker tag $i:latest $i:$version
+    docker tag $i:latest $i:$buildversion
+done
+#push VERSION to git
+git add VERSION
+git commit -m "updated latest version"
+git push 
