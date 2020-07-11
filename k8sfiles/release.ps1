091abdea877e39455e316289ec024919e5da0bb9
pranavkakde <pranavkakde@gmail.com>
2020-07-11 23:39:00 +0930

    fixed release.ps1

---
+++ k8sfiles/release.ps1
@@ -0,0 +1,10 @@
+#Run kubectl to create deployments and services
+
+#kubectl apply -f ./k8sfile/create-map.yaml
+#Create configmap
+kubectl create configmap db-config --dry-run=none -o yaml --from-literal=DBINSTANCE=$Env:DBINSTANCE `
+            --from-literal=DATABASE=$Env:DATABASE  `
+            --from-literal=DBUSER=$Env:DBUSER  `
+            --from-literal=DBPASSWORD=$Env:DBPASSWORD | kubectl apply -f -
+kubectl apply -f .\create-deployment.yaml
+kubectl apply -f .\create-service.yaml
