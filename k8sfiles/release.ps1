3a184db774f7e4b1273d70760b644fe3dc864b3b
pranavkakde <pranavkakde@gmail.com>
2020-07-11 23:55:38 +0930

    updated k8s files

--- k8sfiles/release.ps1
+++ k8sfiles/release.ps1
@@ -2,7 +2,7 @@
 
 #kubectl apply -f ./k8sfile/create-map.yaml
 #Create configmap
-kubectl create configmap db-config --dry-run=none -o yaml --from-literal=DBINSTANCE=$Env:DBINSTANCE `
+kubectl create configmap db-config --dry-run=client -o yaml --from-literal=DBINSTANCE=$Env:DBINSTANCE `
             --from-literal=DATABASE=$Env:DATABASE  `
             --from-literal=DBUSER=$Env:DBUSER  `
             --from-literal=DBPASSWORD=$Env:DBPASSWORD | kubectl apply -f -
